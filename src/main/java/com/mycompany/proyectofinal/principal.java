0806955124f003a344721c3c7cf2a73f7628c32d
julio <julio@172.16.115.61>
2022-06-02 11:58:01 -0500

    Agregsmos un nuevo comentario

--- src/main/java/com/mycompany/proyectofinal/principal.java
+++ src/main/java/com/mycompany/proyectofinal/principal.java
@@ -11,5 +11,6 @@ package com.mycompany.proyectofinal;
 public class principal {
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Esta es mi modificacion del proyecto");
     }
 }
